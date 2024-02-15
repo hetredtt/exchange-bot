50e697d5b3c4e6f7da288392c305bad07242b461
hetredtt <125869909+hetredtt@users.noreply.github.com>
2024-02-15 21:44:52 +0500

    Create service.py

---
+++ bot/MenuModule/service.py
@@ -0,0 +1,19 @@
+
+
+async def say_hello(bot, message):
+    hello = """
+Приветствую, этот бот является тестовым заданием EasyByte.
+В его функционал входит конвертация валют, а также приветствие или прощание в зависимости от контекста.
+Введите /help для знакомаста в с командами
+"""
+    await bot.send_message(chat_id=message.chat.id, text=hello)
+
+async def help(bot, message):
+    help = """
+Команда /convert используется для того чтобы конвертировать сумму из одной валюты в другую.
+Пример использования: 
+\"/convert 100 from UZD to UZS\"
+Узнать коды валют можно с помощью команды /codes
+"""
+    await bot.send_message(chat_id=message.chat.id, text=help)
+
