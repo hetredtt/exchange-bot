1d4b059b4f5780d6938062088f9609b3936aaa43
hetredtt <125869909+hetredtt@users.noreply.github.com>
2024-02-15 21:44:57 +0500

    Create api_query.py

---
+++ bot/utils/api_query.py
@@ -0,0 +1,23 @@
+import requests
+
+async def convert(code_from, code_to, amount):
+    url = "https://api.apilayer.com/currency_data/convert"
+
+    params = {
+        'amount': amount,
+        'from': code_from,
+        'to': code_to
+    }
+    headers= {
+    "apikey": "6XEGKOzrnul8W49oxoUrFP72XuLP4g6N"
+    }
+
+    response = requests.request("GET", url, headers=headers, params = params)
+
+    return response.json()['result']
+
+async def codes():
+    url = "http://127.0.0.1:7000/code/list"
+    response = requests.get(url)
+    data = response.json()
+    return data
